b340f510700e49cfea56eae230f0a33814534c0a
Rahul Sajwan <rahulsajwan98@gmail.com>
2026-02-15 19:09:39 +0530

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom'; // ✅ import this
+
+import App from './App.tsx'
 import './index.css';
-import router from './routes'; // ✅ import your router
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App/>
   </StrictMode>
 );
